8275b91d248de08ffc7d85311841af764cc91439
Alex <norther48@list.ru>
2018-02-02 19:03:50 +0300

    Added WYSIWYG, some manage.posts pages with controllers, tag model and some minor stuff

--- resources/views/manage/posts/edit.blade.php
+++ resources/views/manage/posts/edit.blade.php
@@ -4,23 +4,24 @@
     <div class="flex-container m-l-30">
         <div class="columns">
             <div class="column">
-                <h1 class="title">Создание нового поста</h1>
+                <h1 class="title">Редактирование</h1>
             </div>
         </div>
-        <form action="{{route('posts.store')}}" method="POST">
+        <form action="{{route('posts.update', $post->id)}}" method="POST">
             {{csrf_field()}}
+            {{method_field('PUT')}}
             <div class="columns">
                 <div class="column is-three-quarters-desktop">
                     <div class="card">
                         <div class="card-content">
                             <b-field>
-                                <b-select placeholder="Категория">
+                                <b-select placeholder="Категория" v-model="category">
                                     @foreach( $categories as $category )
-                                    <option
-                                        value="{{$category->id}}">{{$category->name}}
+                                        <option
+                                                value="{{$category->id}}">{{$category->name}}
                                     @endforeach
-                                        <input type="hidden" name="category" value="{{$category->id}}"/>
-                                    </option>
+                                            <input type="hidden" name="category" value="{{$category->id}}"/>
+                                        </option>
                                 </b-select>
                             </b-field>
                             <b-field>
@@ -31,13 +32,14 @@
                                 </b-input>
                             </b-field>
 
-                            <slug-widget url="{{url('/')}}" subdirectory="blog" :title="title" @slug-changed="updateSlug"></slug-widget>
-                            <input type="hidden" v-model="slug" name="slug"/>
+                            {{--<slug-widget url="{{url('/')}}" subdirectory="blog" :title="title" @slug-changed="updateSlug"></slug-widget>--}}
+                            {{--<input type="hidden" v-model="slug" name="slug"/>--}}
 
                             <b-field class="m-t-20">
                                 <b-input placeholder="Тело поста..."
                                          type="textarea"
                                          rows="30"
+                                         v-model="content"
                                          name="content">
                                 </b-input>
                             </b-field>
@@ -49,12 +51,12 @@
                         <ul>
                             <li><button
                                         class="button is-success is-fullwidth" >Опубликовать
-                                        <input type="hidden" name="status" value="2"/>
+                                    <input type="hidden" name="status" value="2"/>
                                 </button>
                             </li>
                             <li><button
                                         class="button is-info is-fullwidth m-t-15" >Сохранить как черновик
-                                        <input type="hidden" name="status" value="1"/>
+                                    <input type="hidden" name="status" value="1"/>
                                 </button>
                             </li>
                             <li><a href="{{route('posts.index')}}" class="button is-danger is-fullwidth m-t-15">Отменить</a> </li>
@@ -67,20 +69,20 @@
 @endsection
 
 @section('scripts')
-
-
     <script>
         var app = new Vue({
             el: '#app',
             data: {
-                title: '',
-                slug: '',
+                category: '{!! $post->category_id !!}',
+                title: '{!! $post->title !!}',
+                {{--slug: '{!! $post->slug !!}',--}}
+                content: '{!! $post->content !!}',
                 api_token: '{{Auth::user()->api_token}}'
             },
             methods: {
-                updateSlug: function(val){
-                    this.slug = val;
-                }
+//                updateSlug: function(val){
+//                    this.slug = val;
+//                }
             }
         });
     </script>
